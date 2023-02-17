f0f81e68a276645bbe8f463880976a90efad2cac
earl-stephens <stephensearl@hotmail.com>
2023-02-16 20:14:30 -0700

    Write first test case and run it

--- src/test/java/com/manning/junitbook/ch02/displayname/DisplayNameTest.java
+++ src/test/java/com/manning/junitbook/ch02/displayname/DisplayNameTest.java
@@ -1,10 +1,17 @@
 package com.manning.junitbook.ch02.displayname;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test class showing the @DisplayName annotation.")
 class DisplayNameTest {
 	private SUT systemUnderTest = new SUT();
 	
-	
+	@Test
+	@DisplayName("Our system under test says hello")
+	void testHello() {
+		assertEquals("Hello", systemUnderTest.hello());
+	}
 }
