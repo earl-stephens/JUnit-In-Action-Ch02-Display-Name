bdcbb6c4f457787f231420b67bb0a34c53546722
earl-stephens <stephensearl@hotmail.com>
2023-02-16 20:05:44 -0700

    Set up test class

---
+++ src/test/java/com/manning/junitbook/ch02/displayname/DisplayNameTest.java
@@ -0,0 +1,10 @@
+package com.manning.junitbook.ch02.displayname;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Test class showing the @DisplayName annotation.")
+class DisplayNameTest {
+	private SUT systemUnderTest = new SUT();
+	
+	
+}
